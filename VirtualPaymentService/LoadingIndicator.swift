73270b0c0bb6cb732a90013d1bee28de4cf62b34
lar-x <lar-x@ukr.net>
2024-05-01 21:45:27 +0300

    launchScreen done

---
+++ VirtualPaymentService/LoadingIndicator.swift
@@ -0,0 +1,48 @@
+//
+//  LoadingIndicator.swift
+//  VirtualPaymentService
+//
+//  Created by lar-x on 01.05.2024.
+//
+
+import UIKit
+import NVActivityIndicatorView
+
+class LoadingIndicator: UIView {
+    
+    public static let shared = LoadingIndicator()
+    private var indicator: NVActivityIndicatorView = {
+        
+        let indicator = NVActivityIndicatorView(frame: CGRect.init(x: 0, y: 0, width: 30, height: 30), type: .ballPulse, color: .black)
+        
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        return indicator
+    }()
+    
+    func show(indicator forView: UIView) {
+       
+        indicator.startAnimating()
+        
+        forView.addSubview(indicator)
+        
+        indicator.topAnchor.constraint(equalTo: forView.bottomAnchor, constant: -100).isActive = true
+        indicator.centerXAnchor.constraint(equalToSystemSpacingAfter: forView.centerXAnchor, multiplier: 0).isActive = true
+        indicator.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        indicator.heightAnchor.constraint(equalToConstant: 30).isActive = true
+
+//        indicator.centerYAnchor.constraint(equalToSystemSpacingBelow: forView.centerYAnchor, multiplier: 0).isActive = true
+//        indicator.bottomAnchor.constraint(equalTo: forView.bottomAnchor, constant: 100).isActive = true
+//        indicator.leftAnchor.constraint(equalTo: imageView.leftAnchor, constant: 30).isActive = true
+//        indicator.rightAnchor.constraint(equalTo: forView.rightAnchor, constant: 3000).isActive = true
+         
+        indicator.bringSubviewToFront(forView)
+    }
+    
+    func hide() {
+        
+        indicator.stopAnimating()
+        indicator.removeFromSuperview()
+    }
+}
+
--- VirtualPaymentService/ViewController.swift
+++ VirtualPaymentService/ViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var gradientIV: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        imageView.center = super.view.center
+        self.view.addSubview(imageView)
+        
+        super.view.backgroundColor = .white
         
         let colorOne = UIColor(red: 156 / 255, green: 44 / 255, blue: 243 / 255, alpha: 1.0).cgColor
         let colorTwo = UIColor(red: 58 / 255, green: 73 / 255, blue: 249 / 255, alpha: 1.0).cgColor
         
         let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = self.gradientIV.frame
+        gradientLayer.frame = imageView.frame
         gradientLayer.colors = [colorOne, colorTwo]
         gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
         gradientLayer.cornerRadius = 20.0
         self.view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        LoadingIndicator.shared.show(indicator: self.view)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 100) {
+            LoadingIndicator.shared.hide()
+            
+        }
     }
 }
 
